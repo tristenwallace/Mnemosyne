29cc2c93ff8f0fa47a724d94452a1784ce0be3c9
nicklobosco <nick.lobosco@gmail.com>
2019-11-12 19:22:28 -0500

    post route

--- app.py
+++ app.py
@@ -6,5 +6,9 @@ app = Flask(__name__)
 def hello():
     return "Hello World!"
 
+@app.route('/testPost', methods = ['POST'])
+def update_text():
+    print(request.form)
+
 if __name__ == '__main__':
     app.run()
