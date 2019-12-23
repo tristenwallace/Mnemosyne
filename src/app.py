06bf42c9b0cfc8ff998cd40dced00e784dba62ed
nicklobosco <nick.lobosco@gmail.com>
2019-12-22 23:22:50 -0500

    add utils

--- src/app.py
+++ src/app.py
@@ -1,57 +1,21 @@
 import os
-import requests
-import pickle
-import io
-import json
-import pymongo
 
-from googleapiclient.discovery import build
-from googleapiclient.http import MediaIoBaseDownload
-from google_auth_oauthlib.flow import InstalledAppFlow
-from google.auth.transport.requests import Request
 from flask import Flask
 from flask import request
 from random import choice
+from utils.utils import *
 
 #App constants
-API_URL = 'https://api.groupme.com/v3/bots/post'
-GROUPME_URL = 'https://image.groupme.com/pictures'
-MONGO_URI = os.environ['MONGODB_URI'] + '?retryWrites=false'
-BOT_ID = os.environ['BOT_ID']
-ACCESS_KEY = os.environ['ACCESS_KEY']
 BOT_NAME = 'Test'
 
 #Flask App
 app = Flask(__name__)
 
-client = pymongo.MongoClient(MONGO_URI)
-db = client.get_default_database()
+#Initiate connections and get files
+db = getMongoDb()
+driveService = getGoogleService(db)
+fileDict = getFiles(driveService)
 
-#get google drive credentials
-creds = None
-dbCreds = db.creds.find_one({})
-if(dbCreds):
-    creds = pickle.loads(dbCreds['rawCreds'])
-    print('creds loaded')
-#TODO - add mechanism to refresh creds
-#if not creds or not creds.valid:
-
-#initiate google drive service
-driveService = build('drive', 'v3', credentials=creds)
-
-#open dictionary
-fileDict = {}
-results = driveService.files().list(fields='nextPageToken, files(id, name, description)').execute()
-for file in results.get('files', []):
-    desc = ''
-    if 'description' in file:
-        desc = file['description']
-    fileDict[file['id']] = {
-        'name': file['name'],
-        'description': desc
-    }
-
-#Routes
 @app.route('/message', methods = ['POST'])
 def handleMessage():
     data = request.get_json()
@@ -59,20 +23,23 @@ def handleMessage():
     if(data['name'] != BOT_NAME and text.startswith('@mnem')):
         if('send a memory' in text):
             sendPic()
-        elif('add a goal' in text):
-            addGoal()
+        elif('add a goal:' in text):
+            addGoal(data['name'], text)
         elif('list goals' in text):
             listGoals()
         elif('help' in text):
             listHelp()
     return 'good', 200
 
-def addGoal():
-    pass
+def addGoal(name, text):
+    goal = ''.join(text.split('add a goal:')[1:]).strip()
+    week = getCurrentWeek()
+    print(week[0], week[1])
+    print(goal)
 
 def listGoals():
     pass
-    
+
 def listHelp():
     helpText = '''
         Use the following commands:
@@ -85,31 +52,12 @@ def listHelp():
         'bot_id': BOT_ID,
         'text': helpText
     }
-    r = requests.post(url = API_URL,data = data)
-
-# param: google drive id for a single file
-# return: url of image stored from groupme service
-# downloads file from google drive, uploads to groupme and returns url of image to send
-def getImageUrl(fileId):
-    if('imageUrl' in fileDict[fileId]):
-        return fileDict[fileId]['imageUrl']
-    req = driveService.files().get_media(fileId=fileId)
-    fh = io.BytesIO()
-    downloader = MediaIoBaseDownload(fh, req)
-    done = False
-    while done is False:
-        status, done = downloader.next_chunk()
-    fh.seek(0)
-    res = requests.post(url=GROUPME_URL, data=fh,
-            headers={'Content-Type': 'image/jpeg','X-Access-Token': ACCESS_KEY})
-    imageUrl = res.json()['payload']['url']
-    fileDict[fileId]['imageUrl'] = imageUrl
-    return imageUrl
+    post(data)
 
 #sends a single picture
 def sendPic():
     fileId = choice(list(fileDict.keys()))
-    imageUrl = getImageUrl(fileId)
+    imageUrl = getImageUrl(fileId, fileDict, driveService)
     text = ''
     if('description' in fileDict[fileId]):
         text = fileDict[fileId]['description']
@@ -122,7 +70,7 @@ def sendPic():
             'url': imageUrl
         }]
     }
-    r = requests.post(url = API_URL,data = data)
+    post(data)
 
 if __name__ == '__main__':
     app.run()
---
+++ src/utils/utils.py
@@ -0,0 +1,90 @@
+import os
+import requests
+import pickle
+import io
+import pymongo
+
+from googleapiclient.discovery import build
+from googleapiclient.http import MediaIoBaseDownload
+from google_auth_oauthlib.flow import InstalledAppFlow
+from google.auth.transport.requests import Request
+from datetime import datetime, timedelta, date
+
+GROUPME_URL = 'https://image.groupme.com/pictures'
+MONGO_URI = os.environ['MONGODB_URI'] + '?retryWrites=false'
+API_URL = 'https://api.groupme.com/v3/bots/post'
+BOT_NAME = 'Test'
+BOT_ID = os.environ['BOT_ID']
+GROUPME_ACCESS_KEY = os.environ['ACCESS_KEY']
+
+def getCurrentWeek():
+    today = date.today()
+    startOfWeek = today - timedelta(days=date_obj.weekday())  # Monday
+    endOfWeek = startOfWeek + timedelta(days=6)  # Sunday
+    return (startOfWeek.strftime('%Y-%m-%d'), endOfWeek.strftime('%Y-%m-%d'))
+
+# param db: a mongodb connection
+# gets google drive credentials from mongodb
+def getGoogleCreds(db):
+    creds = None
+    dbCreds = db.creds.find_one({})
+    if(dbCreds):
+        creds = pickle.loads(dbCreds['rawCreds'])
+        return creds
+    #TODO - add mechanism to refresh creds
+    #if not creds or not creds.valid:
+
+# param db: a mongodb connection
+# returns a google drive service
+def getGoogleService(db):
+    creds = getGoogleCreds(db)
+    return build('drive', 'v3', credentials=creds)
+
+# returns a mongo db connection
+def getMongoDb():
+    #TODO - Add error checking
+    client = pymongo.MongoClient(MONGO_URI)
+    db = client.get_default_database()
+    return db
+
+# param driveService: google drive service
+# returns a dictionary from fileId to name and description
+# gets a list of file metadatas stored in google drive
+def getFiles(driveService):
+    fileDict = {}
+    results = driveService.files().list(fields='nextPageToken, files(id, name, description)').execute()
+    for file in results.get('files', []):
+        desc = ''
+        if 'description' in file:
+            desc = file['description']
+        fileDict[file['id']] = {
+            'name': file['name'],
+            'description': desc
+        }
+    return fileDict
+
+# param fileId: google drive id for a single file
+# param fileDict: dictionary of file infos
+# param driveService: google drive service
+# return: url of image stored from groupme service
+# downloads file from google drive, uploads to groupme and returns url of image to send
+def getImageUrl(fileId, fileDict, driveService):
+    if('imageUrl' in fileDict[fileId]):
+        return fileDict[fileId]['imageUrl']
+    req = driveService.files().get_media(fileId=fileId)
+    fh = io.BytesIO()
+    downloader = MediaIoBaseDownload(fh, req)
+    done = False
+    while done is False:
+        status, done = downloader.next_chunk()
+    fh.seek(0)
+    res = requests.post(url=GROUPME_URL, data=fh,
+            headers={'Content-Type': 'image/jpeg','X-Access-Token': GROUPME_ACCESS_KEY})
+    imageUrl = res.json()['payload']['url']
+    fileDict[fileId]['imageUrl'] = imageUrl
+    return imageUrl
+
+#param data: dictionary of metadata and info to send
+#Posts given data to groupme
+def post(data):
+    requests.post(url = API_URL,data = data)
